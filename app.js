f9095fa77be01ef64dd2cfd7812a8a8bc774ec79
Thomas Warford <thomasdwarford@hotmail.co.uk>
2021-03-27 17:32:30 +0000

    Full pipeline test :)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Deez nuts!'));
+app.get('/', (req, res) => res.send('This change will be manually reviewed.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
